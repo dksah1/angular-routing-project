84d69bab9bff2eab64cbda2cb6babfd1b2e8b6f5
dksah1 <dkboss127126@gmail.com>
2024-03-09 10:27:31 +0545

    routing setup in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { CompanyComponent } from './company/company.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', title: 'Home', component: HomeComponent },
+  { path: 'about', title: 'About', component: AboutComponent },
+  {'path':'about',children:[
+    {'path':'company','title':'About| Company',component:CompanyComponent},
+    {'path':'employees','title':'About|Employees',component:EmployeesComponent}
+  ]},
+  { path: 'contact', title: 'Contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },
+];
